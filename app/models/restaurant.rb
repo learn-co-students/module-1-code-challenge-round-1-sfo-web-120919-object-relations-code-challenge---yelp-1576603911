8c17070fc51fd95eac8dc65b8ef69a629be36bff
github username <github email address>
2019-12-17 11:05:22 -0800

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,33 @@
+require 'pry'
+
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    reviews = Review.all.select {|r| r.restaurant == self}
+  end
+
+  def customers
+    customers  = reviews.map {|r| r.customer}.uniq
+  end
+
+
+  def average_star_rating
+    sum_total = Review.all.sum do |restaurant|
+      restaurant.rating
+    end
+    sum_total / Review.all.size.to_f
   end
 
 end
