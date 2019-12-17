ef107d72cd47d574e771576a347935354f122fb7
github username <github email address>
2019-12-17 11:00:36 -0800

    Assignment

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,67 @@
 class Customer
   attr_accessor :given_name, :family_name
-
+  @@customer_all = []
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@customer_all << self
+############
+    # given_new_name = self.class.format_name(given_name)
+    # family_new_name = self.class.format_name(family_name)
+############
   end
 
   def full_name
     "#{given_name} #{family_name}"
+  end
+
+  def self.all
+    @@customer_all
+  end
+  
+  def my_reviews
+    Review.all.select{|element| element.customer == self}
+  end
+
+  def restaurants
+    restaurant_arr = my_reviews.map{|element| element.restaurant}
+    restaurant_arr.uniq
+  end
+
+  def add_review(restaurant,rating)
+    Review.new(self,restaurant,rating)
+  end
+
+  def num_reviews
+    my_reviews.count
+  end
+
+  def self.format_name(name)
+    arr = name.split(" ")
+    new_arr = arr.map(&:capitalize)
+    new_arr.join(" ")
+  end 
+
+  def self.find_by_name(name)
+    # new_name = self.format_name(name)
+    new_name = name
+    found_name = self.all.find{|element| element.full_name == new_name}
+    if found_name
+      return found_name.given_name
+    else
+      return "No such customer"
+    end
+  end
+
+  def self.find_all_by_given_name(name)
+    # new_name = self.format_name(name)
+    new_name = name
+    new_arr = []
+    self.all.each do |ele|
+      if ele.given_name == new_name
+        new_arr << ele
+      end
+    end
+    new_arr
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,23 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+#############################################
+customer1 = Customer.new("George","Washington")
+customer2 = Customer.new("Bob","Tammy")
+customer3 = Customer.new("Bob","Tammy")
+customer4 = Customer.new("Bob","Tammy")
+
+rest1 = Restaurant.new("Whipping Tales")
+rest2 = Restaurant.new(" My Kitchen ")
+
+review1 = Review.new(customer1,rest1,5)
+review2 = Review.new(customer2,rest1,4.5)
+review3 = Review.new(customer1,rest1,2)
+
+
+
+
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
