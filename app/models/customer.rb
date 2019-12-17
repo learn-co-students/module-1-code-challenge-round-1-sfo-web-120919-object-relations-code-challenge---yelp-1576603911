84979bcf2b4a808954a8b4ba13512443d2d95ef4
brookebachman00 <yourpetlover4411@yahoo.com>
2019-12-17 10:58:35 -0800

    i am disappointed and couldnt remember what i studied

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,46 @@
 class Customer
   attr_accessor :given_name, :family_name
+  @@all = []
 
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self
   end
 
   def full_name
-    "#{given_name} #{family_name}"
+    "#{given_name} + ' ' + #{family_name}"
   end
+
+  def given_name
+    @given_name
+  end
+
+  def family_name
+    @family_name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def restaurants
+    review.restaurants.select do |cust_review|
+      cust_review.restaurants == self
+    end
+  end
+
+  def add_review(restaurant, rating)
+    Review.new(self, restaurant, rating)
+  end
+
+  def num_reviews
+    total = 0
+    review.customer.select do |cust|
+      binding.pry
+      
+    end
+  end
+
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 class Review
+   
+    attr_accessor :rating
+    attr_reader :customer, :restaurant
+    @@all = []
   
+    def initialize(customer,restaurant,rating)
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def customer
+        @customer
+    end
+
+    def restaurant
+        @restaurant
+    end
+
+    def add_review
+
+    end
+
+
 end
