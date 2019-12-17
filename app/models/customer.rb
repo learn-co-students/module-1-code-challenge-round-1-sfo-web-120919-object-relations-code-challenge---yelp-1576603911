f4e1f656ad09eb3d108fec93fed22b87d2f505d4
github username <github email address>
2019-12-17 10:55:34 -0800

    Implemented logic for coding challenge

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,45 @@
 class Customer
   attr_accessor :given_name, :family_name
 
+  @@all = []
+
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    @@all << self
   end
 
   def full_name
     "#{given_name} #{family_name}"
+  end
+
+  def self.all
+    @@all 
+  end
+
+  def restaurants
+    Review.all.map { |review| review.restaurant }.uniq
+  end
+
+  def add_review(restaurant, rating)
+    Review.new(self, restaurant, rating)
+  end
+
+  def reviews
+    Review.all.select do |review| 
+      review.customer == self
+    end
+  end
+
+  def num_reviews
+    reviews.count
+  end
+
+  def self.find_by_name(name)
+    Customer.all.find { |customer| customer.full_name == name}
+  end
+
+  def self.find_all_by_given_name(name)
+    Customer.all.select {|customer| customer.given_name == name}
   end
 end
